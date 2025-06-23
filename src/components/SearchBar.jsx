7e8fd0323311a8707db47b85fda5e5765189b45c
Dritan <dritanrexhepi04@gmail.com>
2025-06-23 22:53:23 +0200

    Created a component, page and context. Also installed tailwindcss

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+function SearchBar () {
+    return (
+        <input 
+        type="text"
+        placeholder="Enter location"
+        className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 shadow-nd"
+        />
+    )
+}
+
+export default SearchBar
---
+++ src/context/WeatherContext.jsx
@@ -0,0 +1,10 @@
+import {createContext, useState, useEffect, useContext} from 'react'
+import {Axios} from "axios";
+
+export const WeatherContext = createContext()
+
+export const WeatherProvider = ({children}) => {
+    return (
+        <></>
+    )
+}
